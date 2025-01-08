0a45002935453abd859d8d159479b5c96a8a4a25
Bimal Kunwar <bimalknwr3@gmail.com>
2025-01-08 23:51:06 +0545

    Add files via upload

---
+++ OOP-LAB/Second lab/uof.cpp
@@ -0,0 +1,27 @@
+// Demonstrate unary operator overloading using friend.     function 
+
+#include <iostream>
+using namespace std;
+class Student {
+private:
+    int age;  
+public:
+    Student(int age){
+        this->age = age;
+    }
+    friend Student& operator++(Student& s);
+    void display() {
+        cout << "Age = " << age << endl;
+    }
+};
+Student& operator++(Student& s) {
+    ++s.age;  
+    return s;
+}
+
+int main() {
+    Student s(30);
+    ++s;  
+    s.display(); 
+    return 0;
+}
---
+++ OOP-LAB/Second lab/uowf.cpp
@@ -0,0 +1,25 @@
+//Demonstrate unary operator overloading (without
+ //friend function)
+
+#include <iostream>
+using namespace std;
+class Student {
+public:
+    int age;
+    Student(int age) {
+        this->age = age;
+    }
+    Student& operator++() {
+        ++age; 
+        return *this;
+    }
+    void display()  {
+        cout << "Age = " << age << endl;
+    }
+};
+int main() {
+    Student s(34); 
+    ++s;  
+    s.display();  
+    return 0;
+}
